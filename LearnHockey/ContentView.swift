b94e90d97eadf5e1daf20b785f2c7bb6cb629c3a
Patrick Fischer <patrick-fischer@live.de>
2020-02-17 15:07:13 -0300

    create hockey app

---
+++ LearnHockey/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  LearnHockey
+//
+//  Created by Patrick Fischer on 17.02.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, World!")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
