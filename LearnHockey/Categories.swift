00253ae684155d720a334dbb07412efa8878e266
Patrick Fischer <patrick-fischer@live.de>
2020-02-17 15:11:24 -0300

    add models

---
+++ LearnHockey/Categories.swift
@@ -0,0 +1,18 @@
+//
+//  Categories.swift
+//  LearnHockey
+//
+//  Created by Patrick Fischer on 17.02.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import Foundation
+
+
+enum Categories {
+    case offense
+    case defense
+    case midfield
+}
+
+
---
+++ LearnHockey/Exercise.swift
@@ -0,0 +1,15 @@
+//
+//  Exercise.swift
+//  LearnHockey
+//
+//  Created by Patrick Fischer on 17.02.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import Foundation
+
+struct Exercise {
+    let name: String
+    let image: URL
+    let explanation: String
+}
