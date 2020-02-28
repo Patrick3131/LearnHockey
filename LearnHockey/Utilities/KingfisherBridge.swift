157e35b49bf1288ce6d6c456c05be89a72f3da6b
Patrick Fischer <patrick-fischer@live.de>
2020-02-28 13:39:25 -0300

    add Kingfisher to project, conflicting library with swiftui import

---
+++ LearnHockey/Utilities/KingfisherBridge.swift
@@ -0,0 +1,18 @@
+//
+//  KingfisherBridge.swift
+//  LearnHockey
+//
+//  Created by Patrick Fischer on 28.02.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import Foundation
+
+import Foundation
+import KingfisherSwiftUI
+
+
+//Kingfisher contains some general purpose types like Kingfisher.Image and Kingfisher.View. If you import the whole SwiftUI and Kingfisher at the same time, you will suffer from a name conflicting. To avoid that, only import the necessary types from Kingfisher, such as import struct Kingfisher.KFImage or import class Kingfisher.KingfisherManager.
+//https://github.com/onevcat/Kingfisher/wiki/SwiftUI-Support
+
+typealias CachedImage = KFImage
