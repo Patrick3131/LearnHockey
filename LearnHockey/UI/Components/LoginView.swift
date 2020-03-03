b23b2b429e9224c24c3d0efa4dec9370ad87fd16
Patrick Fischer <patrick-fischer@live.de>
2020-03-03 13:44:59 -0300

    add LoginView

---
+++ LearnHockey/UI/Components/LoginView.swift
@@ -0,0 +1,21 @@
+//
+//  LoginView.swift
+//  LearnHockey
+//
+//  Created by Patrick Fischer on 01.03.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+struct LoginView: View {
+    var body: some View {
+        FUIAuthBaseViewControllerWrapper()
+    }
+}
+
+struct LoginView_Previews: PreviewProvider {
+    static var previews: some View {
+        LoginView()
+    }
+}
