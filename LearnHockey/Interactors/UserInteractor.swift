381eb10cd670a2f21e6bb4cf8fdd3b76854a93a1
Patrick Fischer <patrick-fischer@live.de>
2020-03-06 21:16:16 -0300

    add empty user to firestore when user does not exist in database when logging in

--- LearnHockey/Interactors/UserInteractor.swift
+++ LearnHockey/Interactors/UserInteractor.swift
@@ -45,6 +45,7 @@ struct AppUserInteractor: UserInteractor {
             promise(.success(true))
         }.eraseToAnyPublisher()
     }
+    
     func createPremium() {
         let userID = appState[\.userData.accountDetails].value?.userUID
         let user = appState[\.userData.accountDetails].value
@@ -63,14 +64,15 @@ struct AppUserInteractor: UserInteractor {
     
     private func listenPremium(user: AccountDetails) {
         let cancelBag = CancelBag()
-                
+        
         premiumRepository.listenPremium(user: user.userUID ?? "1234", completion: { value in
             value.map { value in
                 return Loadable<AccountDetails>.loaded(AccountDetails(accountDetails: user, isPremiumUser: value))
-                }.eraseToAnyPublisher()
-                .sinkToLoadable {self.appState[\.userData.accountDetails] = $0.value! }
+            }
+            .eraseToAnyPublisher()
+            .sinkToLoadable {self.appState[\.userData.accountDetails] = $0.value! }
             .store(in: cancelBag)
-            })
+        })
     }
     
     func loginState() {
@@ -78,22 +80,23 @@ struct AppUserInteractor: UserInteractor {
         let accountDetails = appState.value.userData.accountDetails.value
         appState[\.userData.accountDetails] = .isLoading(last: accountDetails, cancelBag: cancelBag)
         
-        
-        
         authRepository.checkLoginState() { value in
-                   let userDetails = value.flatMap { value in
-                    self.premiumRepository.readPremium(user: value.userUID)
-                   }.eraseToAnyPublisher()
-                   let newValue = value.combineLatest(userDetails) { value, value2 -> AccountDetails in
-                    if value.loggedIn {
-                        let details = AccountDetails(userUID: value.userUID, name: value.name, loggedIn: value.loggedIn, premiumUser: value2)
-                        self.listenPremium(user: details)
-                    }
-                       return AccountDetails(userUID: value.userUID, name: value.name, loggedIn: value.loggedIn, premiumUser: value2)
-                   }.eraseToAnyPublisher()
-                newValue.sinkToLoadable { self.appState[\.userData.accountDetails] = $0 }
-                       .store(in: cancelBag)
-               }
+            let userDetails = value.flatMap { value in
+                self.premiumRepository.readPremium(user: value.userUID)
+            }
+            .eraseToAnyPublisher()
+            .print()
+            let newValue = value.combineLatest(userDetails) { value, value2 -> AccountDetails in
+                if value.loggedIn {
+                    let details = AccountDetails(userUID: value.userUID, name: value.name, loggedIn: value.loggedIn, premiumUser: value2)
+                    self.listenPremium(user: details)
+                }
+                return AccountDetails(userUID: value.userUID, name: value.name, loggedIn: value.loggedIn, premiumUser: value2)
+            }
+            .eraseToAnyPublisher()
+            newValue.sinkToLoadable { self.appState[\.userData.accountDetails] = $0 }
+                .store(in: cancelBag)
+        }
     }
     
     func logOut() {
--- LearnHockey/Repositories/AuthRepository.swift
+++ LearnHockey/Repositories/AuthRepository.swift
@@ -9,6 +9,7 @@
 import Foundation
 import Combine
 import FirebaseAuth
+import FirebaseFirestore
 
 
 protocol AuthRepository {
@@ -17,21 +18,54 @@ protocol AuthRepository {
 }
 
 
+
 class FirebaseUserRepository: AuthRepository {
     
     var handler: AuthStateDidChangeListenerHandle?
+    
+    private var dbName = "user"
+    private var db: Firestore
+    init() {
+        self.db = Firestore.firestore()
+    }
     
     deinit {
         stopListen()
     }
     
+    
+    func subscriber() {
+        
+    }
+    
     func checkLoginState(completion: @escaping (AnyPublisher<AccountDetails,Error>) -> Void) {
-        self.handler = Auth.auth().addStateDidChangeListener { auth, user in
+        self.handler = Auth.auth().addStateDidChangeListener { [weak self] auth, user in
+            guard let safeSelf = self else { return }
             completion(Future<AccountDetails,Error> { promise in
                 if let user = user {
                     print(user)
                     print(auth)
-                    promise(.success(AccountDetails(userUID: user.uid,name: user.displayName, loggedIn: true, premiumUser: false)))
+
+                    safeSelf.checkIfUserIsInDatabase(user: user.uid) { result in
+                        switch result {
+                        case .success(let isAvailable):
+                            if isAvailable {
+                                 promise(.success(AccountDetails(userUID: user.uid,name: user.displayName, loggedIn: true, premiumUser: false)))
+                            } else {
+                                safeSelf.createEmptyUser(user: user.uid) { result in
+                                    switch result {
+                                    case .success(_):
+                                        promise(.success(AccountDetails(userUID: user.uid,name: user.displayName, loggedIn: true, premiumUser: false)))
+                                    case .failure(let error):
+                                        print(error)
+                                    }
+                                }
+                            }
+                        case .failure(let error):
+                            print(error)
+                        }
+                    }
+
                 } else {
                     promise(.success(AccountDetails(userUID: nil, loggedIn: false, premiumUser: false)))
                 }
@@ -42,6 +76,74 @@ class FirebaseUserRepository: AuthRepository {
     
     func logOut() {
         try? Auth.auth().signOut()
+    }
+    
+    
+    private func checkIfUserIsInDatabase(user id: String, completion: @escaping (Result<Bool,Error>) -> Void) {
+        let docRef = db.collection(dbName).document(id)
+        docRef.getDocument { document, error in
+            if let document = document {
+                if document.exists {
+                    completion(.success(true))
+                } else {
+                    print("This is the first login, user: \(id) will be added to Firestore now.")
+                    completion(.success(false))
+                }
+            }
+        }
+    }
+    
+    
+    private func createEmptyUser(user id: String, completion: @escaping (Result<Bool,Error>) -> Void) {
+        let dic = ["premium":false]
+        db.collection(dbName).document(id).setData(dic, merge: true) { error in
+            if let error = error {
+                print(error.localizedDescription)
+                completion(.failure(error))
+            } else {
+                print("User:\(id) was successfully added to Firestory" )
+                completion(.success(true))
+            }
+        }
+    }
+    
+    private func checkIfUserIsInDatabase(user id: String) -> AnyPublisher<Bool,Error> {
+        return Future<Bool,Error>( { [weak self] promise in
+            guard let safeSelf = self else { return }
+            let docRef = safeSelf.db.collection(safeSelf.dbName).document(id)
+            docRef.getDocument { document, error in
+                if let document = document {
+                    if document.exists {
+                        promise(.success(true))
+                    } else {
+                        print("This is the first login, user: \(id) will be added to Firestore now.")
+                        promise(.success(false))
+                        safeSelf.createEmptyUser(user: id)
+                    }
+                }
+            }
+        }).eraseToAnyPublisher()
+        
+    }
+    
+    
+    
+    private func createEmptyUser(user id: String) -> AnyPublisher<Bool,Error> {
+        return Future<Bool,Error>( { [weak self] promise in
+            guard let safeSelf = self else { return }
+
+            let dic = ["premium":false]
+            safeSelf.db.collection(safeSelf.dbName).document(id).setData(dic, merge: true) { error in
+                if let error = error {
+                    print(error.localizedDescription)
+                    promise(.failure(error))
+                } else {
+                    print("User:\(id) was successfully added to Firestory" )
+                    promise(.success(true))
+                }
+            }
+        }).eraseToAnyPublisher()
+        
     }
     
     func createPremiumUser() {
@@ -56,3 +158,29 @@ class FirebaseUserRepository: AuthRepository {
     }
 }
 
+
+
+
+                    
+//                    safeSelf.checkIfUserIsInDatabase(user: user.uid)
+//                        .sinkToResult { value in
+//                            switch value {
+//                            case .success(let isUserInDatabase):
+//                                if isUserInDatabase {
+//                                    promise(.success(AccountDetails(userUID: user.uid,name: user.displayName, loggedIn: true, premiumUser: false)))
+//                                } else {
+//                                    safeSelf.createEmptyUser(user: user.uid)
+//                                        .sinkToResult { value in
+//                                            switch value {
+//                                            case .success( _):
+//                                                promise(.success(AccountDetails(userUID: user.uid,name: user.displayName, loggedIn: true, premiumUser: false)))
+//                                            case .failure(let error):
+//                                                print(error)
+//                                            }
+//                                    }
+//                                }
+//                            case .failure(let error):
+//                                print(error)
+//                            }
+//
+//                    }
