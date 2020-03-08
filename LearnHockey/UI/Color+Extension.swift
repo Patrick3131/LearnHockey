6eaa2f659994942ffdda0c33fffc69f847c79eed
Patrick Fischer <patrick-fischer@live.de>
2020-03-07 22:01:07 -0300

    add colors to project

---
+++ LearnHockey/UI/Color+Extension.swift
@@ -0,0 +1,16 @@
+//
+//  Color+Extension.swift
+//  LearnHockey
+//
+//  Created by Patrick Fischer on 07.03.20.
+//  Copyright © 2020 Patrick Fischer. All rights reserved.
+//
+
+import SwiftUI
+
+
+extension Color {
+    static let baseDark = Color("baseDark")
+    static let baseLight = Color("baseLight")
+    static let baseMidDark = Color("baseMidDark")
+}
