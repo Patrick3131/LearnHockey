05a2044c59d7581480f3187019df2f47dff900d1
Patrick Fischer <patrick-fischer@live.de>
2020-04-07 22:16:27 +0200

    add scrollview

--- LearnHockey/UI/Components/Account/ProfilView.swift
+++ LearnHockey/UI/Components/Account/ProfilView.swift
@@ -9,24 +9,55 @@
 import SwiftUI
 
 struct ProfilView: View {
+    let viewModel: ViewModel
     var body: some View {
-        VStack {
-            ProfilOverviewView()
-            FeedbackView(action: {
-                
-            })
-            Spacer().frame(height: 50)
-            Button(action: {
-                
-            }, label: {
-                Text("I want to delete my profil")
-            })
+        GeometryReader { geometry in
+            ScrollView {
+                VStack {
+                    ProfilOverviewView(viewModel: self.viewModel.createProfilOverViewViewModel())
+                    FeedbackView(action: {
+                        self.viewModel.sendFeedback()
+                    })
+                    Spacer().frame(height: 50)
+                    Button(action: {
+                        self.viewModel.deleteProfil()
+                    }, label: {
+                        Text("I want to delete my profil").foregroundColor(.black)
+                    })
+                    Spacer().frame(height: 15)
+                }.frame(width: geometry.size.width, alignment: .center)
+                    .frame(minHeight: 750)
+            }.frame(width: geometry.size.width, height: geometry.size.height)
         }
+        
+        
     }
 }
 
+
+extension ProfilView {
+    struct ViewModel {
+        var sendFeedback: () -> Void
+        var deleteProfil: () -> Void
+        func createProfilOverViewViewModel() -> ProfilOverviewView.ViewModel {
+            return ProfilOverviewView.ViewModel(managed: {
+                
+            })
+        }
+        
+    }
+    
+    
+    
+}
+
+
 struct ProfilView_Previews: PreviewProvider {
     static var previews: some View {
-        ProfilView()
+        ProfilView(viewModel: ProfilView.ViewModel(sendFeedback: {
+            
+        }, deleteProfil: {
+            
+        }))
     }
 }
